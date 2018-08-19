7af00171d32539875fb1638b73fe5c7f80cfe4f0
ZekLouis <zeklouis@gmail.com>
2018-08-20 00:11:04 +0200

    update custom script

--- resources/[leaker]/cartrailerscript/client.lua
+++ resources/[leaker]/cartrailerscript/client.lua
@@ -1,16 +1,15 @@
 -- commands
-RegisterCommand("lct", function()
-	TriggerEvent("lct")
-end,false)
-
--- events
-RegisterNetEvent('lct')
-AddEventHandler('lct', function()
-	
+RegisterCommand("trailer", function()
+	dis('hey')
 	local playerped = PlayerPedId()
-    dis("ALLO")
-	
-end)
+
+	local coordA = GetEntityCoords(playerped, 1)
+	local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
+	local targetVehicle = getVehicleInDirection(coordA, coordB)
+	local state = IsVehicleExtraTurnedOn(targetVehicle, 1)
+	SetVehicleExtra(targetVehicle, 1, state)
+	dis("end")
+end,false)
 
 function dis(text)
     TriggerEvent("chatMessage", "", { 0, 0, 0 }, text)
